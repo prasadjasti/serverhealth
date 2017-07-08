60938ea7f8a755cd6f929d5490ea0fe5979e1e15
Jasti <broctagon@Broctagons-MacBook-Pro-3.local>
2017-07-08 16:18:50 +0530

    Bot setup initial

---
+++ app.js
@@ -0,0 +1,23 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: '7928bc27-3a2d-4dd0-b577-429e466dd3cb' ,
+    appPassword: 'kgH6KKPnwrwZ9o44eP8JUcH'
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    console.log(session);
+    session.send("You said: %s", session.message.text);
+});
